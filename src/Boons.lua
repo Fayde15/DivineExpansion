f9f133fd671c4d0b175bc8774389bf723ac77fc6
Your Name <you@example.com>
2025-11-12 22:16:44 +0000

    Added the basic layout for boons (Commented it all so it doesnt actually mess with code)

---
+++ src/Boons.lua
@@ -0,0 +1,24 @@
+-- Creating the boon functions itself
+--game.TraitData.Tyche_Boon = {
+    --InheritFrom = { "BaseTrait", "EarthBoon" },
+    --Elements = { "Earth" }, -- Need to add this even if you inherit
+    --Name = "Tyche_Attack_Boon",
+    --BoonInfoTitle = "Tyche_Attack_",
+   -- Icon = "Boon_Tyche_TycheWeaponBoon",
+   -- Slot = "Melee",
+   -- TraitOrderingValueCache = 50,
+  --  Cost = 30,
+  --  BlockStacking = false,
+  --  RarityLevels = {
+  --      Common = {
+   --         Multiplier = 1.00,
+   --     },
+   --     Rare = {
+   --         Multiplier = 1.50,
+   --     },
+   --     Epic = {
+   --         Multiplier = 2.00,
+    --    },
+   --     Heroic = {
+   --         Multiplier = 2.50,
+   --     },
