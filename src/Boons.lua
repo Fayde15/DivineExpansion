703e263f5062498eb562df873212670694017823
Fayde15 <dragogamer6565@gmail.com>
2025-11-26 16:35:22 -0500

    Implement TycheWeaponBoon Boon code w/API format
    Created the TycheWeaponBoon with various attributes and effects.

--- src/Boons.lua
+++ src/Boons.lua
@@ -1,24 +1,86 @@
--- Creating the boon functions itself
---game.TraitData.Tyche_Boon = {
-    --InheritFrom = { "BaseTrait", "EarthBoon" },
-    --Elements = { "Earth" }, -- Need to add this even if you inherit
-    --Name = "Tyche_Attack_Boon",
-    --BoonInfoTitle = "Tyche_Attack_",
-   -- Icon = "Boon_Tyche_TycheWeaponBoon",
-   -- Slot = "Melee",
-   -- TraitOrderingValueCache = 50,
-  --  Cost = 30,
-  --  BlockStacking = false,
-  --  RarityLevels = {
-  --      Common = {
-   --         Multiplier = 1.00,
-   --     },
-   --     Rare = {
-   --         Multiplier = 1.50,
-   --     },
-   --     Epic = {
-   --         Multiplier = 2.00,
-    --    },
-   --     Heroic = {
-   --         Multiplier = 2.50,
-   --     },
+gods.CreateBoon({
+	pluginGUID = _PLUGIN.guid,
+	internalBoonName = "TycheWeaponBoon",  -- eg TycheWeaponBoon
+	isLegendary = false,
+    Elements = {"Air"},
+    characterName = "Tyche",
+	--[[addToExistingGod = { boonPosition = 2 }, -- OR true // Allows you to insert traits into Olympains (Zeus etc) and Daedalus Hammers (characterName = Weapon) --- Chaos and Selene NOT supported ATM.]]
+    --[[? Everything below is an Optional field]]
+
+    -- "Melee" or "Secondary" or "Ranged" or "Rush" or "Mana", doesn't need to be passed if not a core boon.
+    Slot = "Special", 
+    BlockStacking = false, -- Can't be upgraded with poms
+    StatLines = { "NearbyDamageStatDisplay1" }, -- The display for damage bonus etc etc.
+    ExtractValues = { -- Values to get from ExtraFields eg any damage bonus, which is ued in description and in the stat display.
+        {
+            Key = "ReportedWeaponMultiplier",
+            ExtractAs = "TooltipBonus",
+            Format = "PercentDelta",
+            SkipAutoExtract = true,
+		},
+    }, -- refer to traits, or make your own from extra fields
+    displayName = "Awesome!",
+    description = "Awesome Description!",
+    flavourText = "Legendary Flavour Text!",
+    reusingBaseIcons = false, -- Optional, allows you to reuse icons in base game, you still need to pass in paths
+    boonIconPath = "DET-DivineExpansion\\Tyche\\Tyche_Boon",
+    RarityLevels = {
+        Common = { Multiplier = 1.0 },
+        Rare = { Multiplier = 1.5 },
+        Epic = { Multiplier = 2.0 },
+        Heroic = { Multiplier = 2.5 },
+    },
+
+    --[[
+    This is basically where you do all your funky stuff that you want the boon to do, there are a -
+        lot of fields you can look at by using existing traits, or even make your own new ones.
+    ]]
+	ExtraFields = { 
+		AddOutgoingDamageModifiers = {
+			ValidWeapons = WeaponSets.HeroPrimaryWeapons,
+			ConditionalValidWeapon = 
+			{
+				TraitName = "TransformPrimaryTalent",
+				WeaponName = "WeaponTransformAttack"
+			},
+			ProximityThreshold = 430,
+			ProximityThresholdExclusionBoon = "AllCloseBoon",
+			ProximityMultiplier = 
+			{ 
+				BaseValue = 5.0,
+				SourceIsMultiplier = true,
+
+				AbsoluteStackValues =
+				{
+					[1] = 1.25,
+					[2] = 1.15,
+					[3] = 1.10,
+				},
+			},
+			ReportValues = 
+			{ 
+				ReportedWeaponMultiplier = "ProximityMultiplier",
+			},
+		},
+		StatLines =
+		{
+			"NearbyDamageStatDisplay1",
+		},
+		ExtractValues =
+		{
+			{
+					Key = "ReportedWeaponMultiplier",
+					ExtractAs = "TooltipDamageBonus",
+					Format = "PercentDelta",
+			},
+		},
+		WeaponDataOverride = 
+		{
+			WeaponSuitCharged = 
+			{
+				BlockGraphic = "SuitDeflect_Aphrodite",
+			}
+		},
+	}	
+} 
+   )
